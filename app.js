338a2cacfdbb75d060a1c63503c5e02bf95f0a63
Sajees XPS <sajees.jeya@gmail.com>
2022-01-13 17:49:06 -0500

    updated HTML

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer'); 
-// const fs = require('fs'); 
-// const generatePage = require('./src/page-template'); 
+const fs = require('fs'); 
+const generatePage = require('./src/page-template'); 
 
-// const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage();
 
 // fs.writeFile('./index.html', pageHTML, err => {
    // if (err) throw err; 
@@ -142,8 +142,17 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+        // if (err) throw err; 
+
+        // console.log('Page created! Check out index.html in this directory to see it!')
+        // }); 
 }); 
+
+
+
 
 
 
